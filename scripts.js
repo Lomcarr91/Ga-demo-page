a64b2f1cd0c629671fa68fa2dd29edf03ec33da8
Lomcarr91 <56019533+Lomcarr91@users.noreply.github.com>
2020-10-09 15:31:18 +0200

    first commit

--- scripts.js
+++ scripts.js
@@ -1,8 +1,11 @@
 window.addEventListener('wheel', handleScroll);
 
+let frame = 0;
+
 function handleScroll(event) {
-    //event.deltaY;
-    let productElm = document.querySelector('.product-demo')
-    console.log(productElm)
-    productElm.style.cssText = "border: border: 5px solid black;";
+	// event.deltaY;
+	let productElm = document.querySelector('.product-demo');
+    productElm.style.cssText = "background-position-x: calc("+ Math.floor(frame) +" * -605px);";
+    frame += 0.1;
+    if(frame > 3) frame = 0;
 }
