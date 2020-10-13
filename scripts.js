4700105d038194807f7f9fa67bb00ce680db1c48
Lomcarr91 <56019533+Lomcarr91@users.noreply.github.com>
2020-10-13 11:15:21 +0200

    andre utkast

--- scripts.js
+++ scripts.js
@@ -3,9 +3,13 @@ window.addEventListener('wheel', handleScroll);
 let frame = 0;
 
 function handleScroll(event) {
-	// event.deltaY;
+	//event.deltaY;
 	let productElm = document.querySelector('.product-demo');
     productElm.style.cssText = "background-position-x: calc("+ Math.floor(frame) +" * -605px);";
     frame += 0.1;
     if(frame > 3) frame = 0;
+
 }
+
+
+//Hvordan 
